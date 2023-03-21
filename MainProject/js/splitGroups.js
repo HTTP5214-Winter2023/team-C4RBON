1601352e52b6abcd206d0fcf8e84da6924ba3b81
Muskan Aggarwal <n01540458@humbermail.ca>
2023-03-21 14:17:32 -0400

    split groups front end

--- MainProject/js/splitGroups.js
+++ MainProject/js/splitGroups.js
@@ -1,11 +1,9 @@
 function studentSplitter(students, size) {
   const shuffleStudents = students.sort(() => Math.random() - 0.5); // Shuffle the students randomly
+  size = parseInt(size);
   const groups = [];
-  let i = 0;
-  while (i < shuffleStudents.length) {
+  for(var i=0; i<shuffleStudents.length; i+=size) {
     groups.push(shuffleStudents.slice(i, i + size));
-    i += size;
   }
-  console.log(groups);
   return groups;
 }
