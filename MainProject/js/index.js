7ba4723a0e6a87924f1153482f765f4d99631b51
Harshadkumar Patel <64983530+Harshpateloo7@users.noreply.github.com>
2023-04-18 22:49:24 -0400

    Updated README file, Updated Validation using JavaScript, Updated output layout as table

--- MainProject/js/index.js
+++ MainProject/js/index.js
@@ -11,6 +11,11 @@ function pageloaded() {
         var studentName = document.getElementById("name");
         var studentNumber = document.getElementById("number");
         var studentEmail = document.getElementById("email");
+
+        if (studentName.value === '' || studentNumber.value === '' || studentEmail.value === ''){
+            alert('Pleasee fill all required field before adding.')
+            return false;
+        }
         var studentData = {
             name: studentName.value,
             number: studentNumber.value,
@@ -19,38 +24,96 @@ function pageloaded() {
 
         arrayStudents.push(studentData);
         //console.log(arrayStudents);
-        var namesArr = [];
-        for(var i=0; i<arrayStudents.length; i++) {
-            namesArr.push(arrayStudents[i]["name"]);
-        }
+        // var namesArr = [];
+        lastStudent = arrayStudents[arrayStudents.length-1]
+        displayData(lastStudent["name"], lastStudent["number"], lastStudent["email"])
+  
 
-        listOfStudents.innerHTML = "List of Students: " + namesArr.join(", ");
-        document.getElementById("name").value = "";
-        document.getElementById("number").value = "";
-        document.getElementById("email").value = "";
+        // listOfStudents.innerHTML = "List of Students: " + namesArr.join(", ");
+        // document.getElementById("name").value = "";
+        // document.getElementById("number").value = "";
+        // document.getElementById("email").value = "";
     }
 
     document.getElementById("submitForm").onsubmit = submitForm;
     //var splitSize = document.getElementById("groupSize").value;
 
-    function submitForm() {
-        var groupsArr = studentSplitter(arrayStudents, document.getElementById("groupSize").value);
-        //console.log(groupsArr);
+function displayData (sname, snumber, semail){
 
-        var groupsNamesArr = [];
-        for(var i=0; i<groupsArr.length; i++) {
-            var namesArr = [];
-            for(var j=0; j<groupsArr[i].length; j++) {
-                namesArr.push(groupsArr[i][j]["name"]);
-            }
-            groupsNamesArr.push("( " + namesArr.join(", ") + " )");
-        }
-        document.getElementById("groupList").innerHTML = "Groups List: " + groupsNamesArr.join(", ");
+    tableBody = document.getElementById("studentData")
 
-        var table = document.getElementById("table");  // set this to your table
+    var row = document.createElement("tr")
+    var name = document.createElement("td")
+    name.textContent = sname
+    row.appendChild(name)
 
+    var id = document.createElement("td")
+    id.textContent = snumber
+    row.append(id)
 
-        return false;
-    }
+    var email = document.createElement("td")
+    email.textContent = semail
+    row.append(email)
+
+    tableBody.appendChild(row)
 
 }
+
+function createTable(arr){
+    parentElem = document.getElementById("groupList")
+    var heads = ["Name", "Number", "Email"]
+
+    var table = document.createElement("table")
+    table.setAttribute("id", "students-list")
+
+    var thead = document.createElement("thead")
+    var tbody = document.createElement("tbody")
+    tbody.setAttribute("id", "studentData")
+
+    var headers = document.createElement("tr")
+
+    heads.forEach((h) => {
+        var th = document.createElement("th")
+        th.textContent = h;
+        headers.append(th);
+
+    })
+
+    thead.appendChild(headers)
+    table.appendChild(thead)
+
+    arr.forEach((i) =>{
+
+        row = document.createElement("tr")
+        
+        nameCell = document.createElement("td")
+        nameCell.textContent = i["name"]
+        row.appendChild(nameCell)
+
+        numberCell = document.createElement("td")
+        numberCell.textContent = i.number
+        row.appendChild(numberCell)
+
+        emailCell = document.createElement("td")
+        emailCell.textContent = i.email
+        row.appendChild(emailCell)
+
+        tbody.appendChild(row)
+
+    })
+
+    table.appendChild(tbody)
+    parentElem.appendChild(table)
+}
+
+
+function submitForm() {
+        parentElem = document.getElementById("groupList")
+        parentElem.innerHTML = ''
+        var groupsArr = studentSplitter(arrayStudents, document.getElementById("groupSize").value);
+        groupsArr.forEach(element => {
+            createTable(element)
+        })
+
+        return false;
+    }}
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Team-C4RBON
+# Team members:
+- Aadrit Chauhan
+- Azmat Pathan
+- Darshan Varma
+- Harshadkumar Patel
+- Jigar Mehta
+- Lukmon Lasisi
+- Muskan Aggarwal
+- Nency Modi
+# Project Idea
+- The Randomized Grouping Web Application is designed to automatically organize group structures after entering details. The system utilizes a randomized algorithm to divide individuals into groups based on user-specified criteria. This application can be used in a variety of settings, including academic environments, workplace settings, and social events.
+# Project 1: Client-Side
+- A client-side project developed using HTML, CSS, and JavaScript.
+- Project directory: MainProject.
+- URL: https://github.com/HTTP5214-Winter2023/team-C4RBON/tree/main/MainProject
+# Project 2: Server-Side
+- A server-side project developed using Node.js, Express, Pug Template, and MongoDB.
+- Project directory: groupProject.
+- URL: https://github.com/HTTP5214-Winter2023/team-C4RBON/tree/main/groupProject
+# User Testing Note:
+- userTesting.md file
+# Meeting Notes:
+- Team meeting notes.md
